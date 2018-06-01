244280902df906b3c6f83ee679fab7686dcd696d
Saicharan Reddy <saicharan.reddy1@gmail.com>
2018-06-01 07:46:10 -0500

    Added files - II

---
+++ forms.py
@@ -0,0 +1,37 @@
+from flask_wtf import Form
+from wtforms import TextField, PasswordField, SelectField
+from wtforms.validators import DataRequired, EqualTo, Length
+
+# Set your classes here.
+
+
+class RegisterForm(Form):
+    name = TextField(
+        'Username', validators=[DataRequired(), Length(min=6, max=25)]
+    )
+    email = TextField(
+        'Email', validators=[DataRequired(), Length(min=6, max=40)]
+    )
+    password = PasswordField(
+        'Password', validators=[DataRequired(), Length(min=6, max=40)]
+    )
+    confirm = PasswordField(
+        'Repeat Password',
+        [DataRequired(),
+        EqualTo('password', message='Passwords must match')]
+    )
+    type = SelectField(
+        'Select Type',
+        choices=[('sub','Subscriber'),('rev','Reviewer'),('pub','publisher')]
+    )
+
+
+class LoginForm(Form):
+    email = TextField('Username', [DataRequired()])
+    password = PasswordField('Password', [DataRequired()])
+
+
+class ForgotForm(Form):
+    email = TextField(
+        'Email', validators=[DataRequired(), Length(min=6, max=40)]
+    )
---
+++ models.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_mongoalchemy import MongoAlchemy
+
+app = Flask(__name__)
+
+app.config['MONGOALCHEMY_DATABASE'] = 'saiop'
+app.config['MONGOALCHEMY_CONNECTION_STRING'] = 'mongodb://admin:freeflow@ds135760.mlab.com:35760/saiop'
+
+db = MongoAlchemy(app)
+
+class User(db.Document):
+    name = db.StringField()
+    email = db.StringField()
+    password = db.StringField()
+    type = db.StringField()
+
+class Journal(db.Document):
+    title = db.StringField()
+    user_email = db.StringField()
+    domain = db.StringField()
+    status = db.StringField()
+    filename = db.StringField()
