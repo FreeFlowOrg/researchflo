e1e221164e53f969fe6bc3e9deece1201c0dcb30
Saicharan Reddy <saicharan.reddy1@gmail.com>
2018-06-09 23:27:40 -0500

    Update config.py

--- config.py
+++ config.py
@@ -15,6 +15,6 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.db')
 
 #AWS credentials
 S3_BUCKET                 = "zappa-jepozm5pw"
-S3_KEY                    = "AKIAJ7FYBZBFOY4DUJ5A"
-S3_SECRET                 = "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"
+S3_KEY                    = ""
+S3_SECRET                 = ""
 S3_LOCATION               = 'http://{}.s3.amazonaws.com/'.format(S3_BUCKET)
